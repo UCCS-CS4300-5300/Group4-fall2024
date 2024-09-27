12e7f472cc3466db20794f297c471653af2f4be9
Brett Walton <58231522+Sloathking@users.noreply.github.com>
2024-09-27 02:30:56 +0000

    Running 'runserver' automatically starts on correct IP and port

--- tlm/tlm/settings.py
+++ tlm/tlm/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.core.management.commands.runserver import Command as runserver
+runserver.default_addr = '0.0.0.0'
+runserver.default_port = '3000'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
