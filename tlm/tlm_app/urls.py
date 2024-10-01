bfc971082d7a8671e0cf9581fd8fb0524a6e918b
UCCS-MDelgado <mdelgad3@uccs.edu>
2024-10-01 00:06:52 +0000

    Added pages for redirection from the index page and redirections to those pages

--- tlm/tlm_app/urls.py
+++ tlm/tlm_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # Index page url
+    path('about/', views.about, name='about'), # About page url
+    path('lists/', views.myLists, name='lists'), # My list page url
+    path('login/', views.login, name='login'), # Login page url
+    path('profile/', views.profile, name='profile'), # Profile page url
+    path('quick_Translate', views.quickTranslate, name='quick_Translate'), # Quick translate page url
+    path('settings', views.settings, name='settings'), # Settings page url 
+    path('signup', views.signup, name='signup') # Sign up page url
 ]
--- tlm/tlm_app/views.py
+++ tlm/tlm_app/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def myLists(request):
+    return render(request, 'lists.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def quickTranslate(request):
+    return render(request, 'quick_translate.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def signup(request):
+    return render(request, 'signup.html')
