9f7bf6a04a389f5d0c099b90554c47547400b962
Aman Kumar <aman7907@gmail.com>
2024-10-17 02:55:48 +0000

    Fixed pronunciation button, dark mode, and forms boxes

--- tlm/tlm_app/forms.py
+++ tlm/tlm_app/forms.py
@@ -7,6 +7,12 @@ class QuickTranslateForm(forms.ModelForm):
     class Meta:
         model = UserTranslationHistory
         fields = ['sourceLanguage', 'sourceText', 'targetLanguage', 'targetText']
+        widgets = {
+            'sourceLanguage': forms.Select(attrs={'class': 'form-control'}),
+            'sourceText': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'targetLanguage': forms.Select(attrs={'class': 'form-control'}),
+            'targetText': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
